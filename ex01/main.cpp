1fe80341eda07e3a9737f82f38051efbe7a12e40
Mari Cruz <maricruz.njie@gmail.com>
2026-03-09 11:42:44 +0100

    add tests

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mari-cruz <mari-cruz@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/06 14:38:47 by mari-cruz         #+#    #+#             */
-/*   Updated: 2026/03/06 14:54:14 by mari-cruz        ###   ########.fr       */
+/*   Updated: 2026/03/09 11:42:24 by mari-cruz        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,17 @@
 
 int main(void)
 {
+
+    Span sp = Span(5);
+    
+    sp.addNumber(6);
+    sp.addNumber(3);
+    sp.addNumber(17);
+    sp.addNumber(9);
+    sp.addNumber(11);
+    std::cout << sp.shortestSpan() << std::endl;
+    std::cout << sp.longestSpan() << std::endl;
+    
     Span huge(20000);
 
     int i = 0;
@@ -24,4 +35,25 @@ int main(void)
     }
     std::cout << huge.shortestSpan() << std::endl;
     std::cout << huge.longestSpan() << std::endl;
+
+    Span small(1);
+
+    try
+    {
+        small.addNumber(17);
+        std::cout << small.shortestSpan() << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    try
+    {
+        small.addNumber(17);
+        std::cout << small.longestSpan() << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
 }
